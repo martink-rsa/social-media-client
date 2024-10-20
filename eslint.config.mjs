375b7e5f4c53c8cd6958e79655b0594f57dbd7f3
Martin Kruger <52622303+martink-rsa@users.noreply.github.com>
2024-10-20 12:37:44 +0200

    Updated ESLint to 9

---
+++ eslint.config.mjs
@@ -0,0 +1,46 @@
+import globals from "globals";
+import jest from "eslint-plugin-jest";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [...compat.extends("eslint:recommended"), {
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+        },
+
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+
+    rules: {},
+}, ...compat.extends("plugin:jest/recommended").map(config => ({
+    ...config,
+    files: ["**/*.test.js"],
+})), {
+    files: ["**/*.test.js"],
+
+    plugins: {
+        jest,
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.jest,
+        },
+    },
+
+    rules: {
+        "jest/prefer-expect-assertions": "off",
+    },
+}];
